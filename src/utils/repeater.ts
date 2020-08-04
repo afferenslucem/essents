e9060ce45a7d1f445c90169f354079e908abcf16
Alex Shakirov <hrodveetnir@protonmail.com>
2020-08-04 08:38:47 +0500

    added repeater

--- src/utils/repeater.ts
+++ src/utils/repeater.ts
@@ -1,3 +1,36 @@
 export class Repeater {
-    
+    private _id: number | null = null;
+    private _args: any[];
+
+    public constructor(private _func: Function, private _period: number, ...args: any[]) {
+        this._args = args;
+    }
+
+    public start(): Repeater {
+        this._id = setInterval(() => {
+            this._func(...this._args);
+        }, this._period);
+
+        return this;
+    }
+
+    public kill(): Repeater {
+        if(this._id == null) {
+            throw new Error('Could not delete empty timer');
+        }
+
+        clearInterval(this._id);
+
+        this._id = null;
+
+        return this;
+    }
+
+    public get alive(): boolean {
+        return this._id != null;
+    }
+
+    public get period(): number {
+        return this._period;
+    }
 }
---
+++ tests/repeater.spec.ts
@@ -0,0 +1,40 @@
+import { Repeater } from "../src/utils/repeater";
+import { assert } from "chai";
+import { Timer } from "../src";
+
+describe('Repeater', () => {
+    it('should create repeater', () => {
+        const repeater = new Repeater(() => {}, 10);
+
+        assert.equal(repeater.alive, false);
+        assert.equal(repeater.period, 10);
+    });
+ 
+    it('repeater should became alive after start', () => {
+        const repeater = new Repeater(() => {}, 10).start();
+
+        assert.equal(repeater.alive, true);
+
+        repeater.kill();
+
+        assert.equal(repeater.alive, false);
+    });
+ 
+    it('repeater should run routine', async () => {
+        let i = 0
+
+        const repeater = new Repeater(() => {
+            i++;
+        }, 10).start();
+
+        const promise = new Promise(resolve => {
+            new Timer(resolve, 50).start();
+        });
+
+        await promise;
+
+        assert.isTrue(i > 2);
+
+        repeater.kill();
+    });
+});
